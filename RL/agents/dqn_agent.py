a2163bc4806dbfde7097d47d7a8f3a5c14180ef1
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2020-05-10 11:26:55 -0400

    grad clip bug fix

--- RL/agents/dqn_agent.py
+++ RL/agents/dqn_agent.py
@@ -145,8 +145,8 @@ class DQNCoreAgent(RL.Agent):
             loss.backward()
             if self.grad_clip is not None:
                 logger.debug('Doing grad clipping')
-                for p in self.q.parameters():
-                    p.grad.data.clamp_(-self.grad_clip, self.grad_clip)
+                torch.nn.utils.clip_grad_norm_(
+                    self.q.parameters(), self.grad_clip)
             self.optim.step()
             loss = loss.cpu().detach().item()
 
