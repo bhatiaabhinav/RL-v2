f0dd63e8e67119187fd105b0b63e4f3d0f84beac
Abhinav Bhatia <bhatiaabhinav93@gmail.com>
2020-05-16 18:00:12 -0400

    Version increment to reflect bug fix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="RL-v2",
-    version="1.1.0b",
+    version="1.1.1b",
     author="Abhinav Bhatia",
     author_email="bhatiaabhinav93@gmail.com",
     description=("Some RL algorithms"),
